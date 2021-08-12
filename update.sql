3139bd0ab5c0faf22f3b8d23e2c093e7c3c720e3
sgvaidyas <69918605+sgvaidyas@users.noreply.github.com>
2021-08-12 13:11:25 +0530

    Add files via upload

---
+++ update.sql
@@ -0,0 +1,22 @@
+﻿create procedure sproc_update
+(	@id int,
+	@name varchar(50),
+	@email varchar(50),
+	@phone varchar(10),
+	@fees float,
+	@per float
+)
+as
+begin
+
+	update Student
+	set STUDENTNAME=@name,EMAIL=@email,PHONE=@phone,FEES=@fees,"PERCENT"=@per
+	where Id=@id;
+end
+
+select * from Student where Id=101;
+
+exec sproc_update 101,'Divya','divya@gmail.com','9876543212',11000,91
+
+
+
